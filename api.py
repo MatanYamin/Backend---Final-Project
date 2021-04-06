713c44e368f8fa3914d85bfd723d819b27938915
MatanYamin <yamin2211@gmail.com>
2021-04-06 12:28:05 +0300

    check

--- api.py
+++ api.py
@@ -265,5 +265,5 @@ def edit_description_for_service():
 
 if __name__ == "__main__":
     # app.run(debug=True, host="3.138.43.76", port=8080)
-    app.run(debug=True)
-    # app.run(host="0.0.0.0", port=8000)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
