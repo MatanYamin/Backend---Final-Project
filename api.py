6eb0c3f65521ad0423e99d8ed527ccbee43f6270
MatanYamin <yamin2211@gmail.com>
2021-04-10 19:13:37 +0300

    Allowing CORS block

--- api.py
+++ api.py
@@ -308,6 +308,7 @@ def add_image():
 if __name__ == "__main__":
     # app.run(debug=True, host="3.138.43.76", port=8080)
     # CORS(app)
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(ssl_context='adhoc')
     # app.run(host='3.138.43.76', port=8080)
     # app.run(host="0.0.0.0", port=8080)
